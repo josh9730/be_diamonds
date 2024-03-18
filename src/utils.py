0497fd6924e4972465fafd2b1f3f2cf515d01eb1
Josh <jdickman@cenic.org>
2024-03-18 16:45:08 -0700

    refactor wip

--- src/utils.py
+++ src/utils.py
@@ -1,5 +1,5 @@
 from datetime import datetime
-from typing import Final
+from typing import Final, Iterable
 
 import yaml
 from pytz import timezone
@@ -17,7 +17,8 @@ def get_sheet_name(_type: str) -> str:
 
 def save_sheet_name(sheet_name: str, _type: str) -> None:
     with open(SHEET_NAME_FILE, "r") as f:
-        data = yaml.safe_load(f)[_type]
+        data = yaml.safe_load(f)
+
     with open(SHEET_NAME_FILE, "w") as f:
         data.update({_type: sheet_name})
         yaml.safe_dump(data, f)
@@ -28,3 +29,7 @@ def save_df_output(df: "pd.DataFrame") -> None:
     df_format = tabulate(df, headers="keys", tablefmt="psql")
     with open("logs/df_output.txt", "w") as f:
         f.write(f"DATE: {TODAY}\n\n{df_format}")
+
+
+def filter_list(_list: list, filter_list: Iterable) -> list:
+    return [i for i in _list if i not in filter_list]
